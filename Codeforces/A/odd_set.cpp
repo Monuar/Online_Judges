ec622e778d1a8ee98aa9d6a98a1b118097ca2e6c
Monuar Hossain <monuar.cste@gmail.com>
2021-07-05 00:02:25 +0600

    initial commit

--- Codeforces/A/odd_set.cpp
+++ Codeforces/A/odd_set.cpp
@@ -5,19 +5,16 @@ int main()
     int t;
     cin>>t;
     while(t--){
-        int n,cnt=0;
+        int n,even=0,odd=0;
         cin>>n;
         int x=2*n;
         int a[x];
         for(int i=0;i<x;i++){
             cin>>a[i];
-            if(a[i]%2!=0) cnt++;
+            if(a[i]%2!=0) odd++;
+            else even++;
         }
-        if(cnt>0&&cnt%n==0){
-            cnt=cnt/n;
-            if(cnt%2!=0) cout<<"Yes"<<endl;
-            else cout<<"No"<<endl;
-        }
+        if(even==odd) cout<<"Yes"<<endl;
         else cout<<"No"<<endl;
     }
 }
