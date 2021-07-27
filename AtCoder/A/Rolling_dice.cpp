9ff859dec756dc3a5c21c46b7657fb48e41ca708
Monuar Hossain <monuar.cste@gmail.com>
2021-07-27 10:23:42 +0600

    initial commit

---
+++ AtCoder/A/Rolling_dice.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll a,b;
+    cin>>a>>b;
+    ll d=ceil(b/6.0);
+    if(d<=a) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
---
+++ AtCoder/A/kcal.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //cout<<fixed<<setprecision(1);
+    int a,b;
+    cin>>a>>b;
+    double ans=(b/100.0)*a;
+    cout<<ans<<endl;
+}
