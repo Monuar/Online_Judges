a0575bcc4d50914a62244f5a30df23b7a74b8bba
Monuar Hossain <monuar.cste@gmail.com>
2021-07-28 20:17:17 +0600

    initial commit

--- AtCoder/B/palindrome_with_leading_zeros.cpp
+++ AtCoder/B/palindrome_with_leading_zeros.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 int main()
 {
-    string s,s1,s2;
-   // s1=s;
+    string s,s1,s2="";
     cin>>s;
     s1=s;
     reverse(s.begin(),s.end());
-    //cout<<"s1 "<<s1<<endl;
     int cnt=0;
     for(int i=0;i<s.size();i++){
-        if(s[i]==0){
+        if(s[i]=='0'){
             cnt++;
         }
         else break;
@@ -18,11 +16,8 @@ int main()
     for(int i=0;i<s1.size()-cnt;i++){
         s2+=s1[i];
     }
-    //cout<<s2<<endl;
     string s3=s2;
     reverse(s2.begin(),s2.end());
-    //cout<<s3<<" "<<s2<<endl;
     if(s2==s3) cout<<"Yes"<<endl;
     else cout<<"No"<<endl;
-
 }
